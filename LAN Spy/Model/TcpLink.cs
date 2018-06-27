2705f9f6e64661bf65e67b64d0c7876442cbd548
熊某人 <840646587@qq.com>
2018-06-27 21:33:19 +0800

    分离TcpLink类

---
+++ LAN Spy/Model/TcpLink.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Net;
+
+namespace LAN_Spy.Model {
+
+    /// <summary>
+    ///     网路上一组TCP连接的两端主机。
+    /// </summary>
+    public class TcpLink {
+        /// <summary>
+        ///     初始化 <see cref="TcpLink" /> 类的实例。
+        /// </summary>
+        /// <param name="src">连接的起点，推荐以当前子网的主机作为起点。</param>
+        /// <param name="dst">连接的终点，推荐以非当前子网的主机作为终点。</param>
+        public TcpLink(IPAddress src, IPAddress dst) {
+            Src = src;
+            Dst = dst;
+            UpdateTime();
+        }
+
+        /// <summary>
+        ///     连接的起点。
+        /// </summary>
+        public IPAddress Src { get; }
+
+        /// <summary>
+        ///     连接的终点，
+        /// </summary>
+        public IPAddress Dst { get; }
+
+        /// <summary>
+        ///     最后检测到连接的时间，并非确切的连接开始时间，而是侦测到连接时的本地时间。
+        /// </summary>
+        public DateTime Time { get; private set; }
+
+        /// <summary>
+        ///     更新最后检测到连接的时间。
+        /// </summary>
+        public void UpdateTime() {
+            Time = DateTime.Now;
+        }
+    }
+}
